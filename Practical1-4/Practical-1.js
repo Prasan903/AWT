4075257ea21ab3064c7dbd7349894d73392468c0
21CE121 Prasan <110338668+Prasan903@users.noreply.github.com>
2023-07-28 13:38:56 +0530

    Create Practical-1.js

---
+++ Practical1-4/Practical-1.js
@@ -0,0 +1,15 @@
+const str = "Prasan Savaliya!";
+const flag = true;
+const num = 50;
+
+console.log("Str:", str);
+console.log("Num:", num);
+console.log("Flag:", flag);
+
+const temp1 = 5, temp2 = 10;
+const sum = addNumbers(temp1, temp2);
+console.log("Sum:", sum);
+
+function addNumbers(temp1, temp2) {
+    return temp1 + temp2;
+}
